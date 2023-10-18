937efba0b1c5a1aa0e6c1dcb9ea0a1c95afb4304
ml-srv <bagger546@yandex.ru>
2023-10-18 20:50:13 +0500

    changes

--- model_preparation.py
+++ model_preparation.py
@@ -2,12 +2,16 @@
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import pickle
+import yaml
 
+params = yaml.safe_load(open("params.yaml"))["train"]
+C = params["C"]
+penalty = params["penalty"]
 X_train = pd.read_csv('data/train/X_train.csv', delimiter = ',')
 y_train = pd.read_csv('data/train/Y_train.csv', delimiter = ',')
 model = LogisticRegression(fit_intercept=True,
-                            penalty='l1',solver='liblinear',
-                            C=8.75,
+                            penalty=penalty,solver='liblinear',
+                            C=C,
                             max_iter=10000)
 model.fit(X_train, y_train.values.ravel())
 
