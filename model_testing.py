e864c30c4f0872680a7dc2a19b2313c908c8db55
ml-srv <bagger546@yandex.ru>
2023-10-18 21:55:33 +0500

    change params

--- model_testing.py
+++ model_testing.py
@@ -3,7 +3,7 @@ import pickle
 from sklearn.metrics import f1_score
 import json
 
-LogReg = pickle.load(open('model.pkl', 'rb'))
+LogReg = pickle.load(open('models/model.pkl', 'rb'))
 X_test = pd.read_csv('data/test/X_test.csv', delimiter = ',')
 Y_test = pd.read_csv('data/test/Y_test.csv', delimiter = ',')
 y_preds = LogReg.predict(X_test)
